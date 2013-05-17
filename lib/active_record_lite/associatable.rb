5173b96658a4b41d8ac8fbe0d77f2a74d7a6e478
Scott Rogers <scott@iamscottrogers.com>
2013-05-17 16:03:16 -0400

    HasManyAssocParams helper class works

--- lib/active_record_lite/associatable.rb
+++ lib/active_record_lite/associatable.rb
@@ -1,7 +1,24 @@
 require 'active_support/core_ext/object/try'
 require 'active_support/inflector'
 
+  
+class HasManyAssocParams
+  attr_reader :other_class, :other_table_name, :primary_key, :foreign_key, :own_table_name
+
+  def initialize(other, original, params=nil)
+    @other_class = (params[:class_name]|| other.to_s.singularize)
+    @other_class = @other_class.camelize.constantize
+
+    @other_table_name = @other_class.table_name
+    @own_table_name = original.class.table_name
+
+    @primary_key = params[:primary_key] || "id"
+    @foreign_key = params[:foreign_key] || "#{@other_class.to_s.downcase}_id"
+  end
+end
+
 module Associatable
+
   def belongs_to(owner, params=nil)
     define_method(owner) do
       other_class = (params[:class_name].camelize || owner.to_s.camelize ).constantize
@@ -22,24 +39,22 @@ module Associatable
     end
   end
 
-  def has_many(name, params=nil)
-    define_method(name) do
-      other_class = (params[:class_name]|| name.to_s.singularize)
-      other_class = other_class.camelize.constantize
-      other_table_name = other_class.table_name
-      primary_key = params[:primary_key] || "id"
-      foreign_key = params[:foreign_key] || "#{other_class.to_s.downcase}_id"
+  def has_many(other, params=nil)
+    define_method(other) do
+      aps = HasManyAssocParams.new(other, self, params)
 
       query = <<-SQL
         SELECT other.*
           FROM #{self.class.table_name} AS original 
-          JOIN #{other_table_name} AS other
-            ON original.#{primary_key} = other.#{foreign_key}
-         WHERE original.#{primary_key} = ?
+          JOIN #{aps.other_table_name} AS other
+            ON original.#{aps.primary_key} = other.#{aps.foreign_key}
+         WHERE original.#{aps.primary_key} = ?
       SQL
 
+      puts query
+
       result = DBConnection.execute(query, self.id)
-      other_class.parse_all(result)
+      aps.other_class.parse_all(result)
     end
     
   end
